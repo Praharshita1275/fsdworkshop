9e0c86edf5e8407a31c2578e8f46e00cf34ac571
Praharshita1275 <praharshita05@gmail.com>
2025-08-23 15:19:06 +0530

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/services/firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,17 +1,22 @@
-import { initializeApp } from "firebase/app";
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import {getFirestore} from 'firebase/firestore'
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBGtEptn45w6rIxEb3dUhg9KBnnNTWY2_M",
-  authDomain: "fir-dbaa1.firebaseapp.com",
-  projectId: "fir-dbaa1",
-  storageBucket: "fir-dbaa1.firebasestorage.app",
-  messagingSenderId: "493730980912",
-  appId: "1:493730980912:web:e9a566b65a5ec5ef0eb9f5"
+  apiKey: "AIzaSyBelW8a56XHO429HY8rt20rQfMsK1WZ8D4",
+  authDomain: "fsdworkshop.firebaseapp.com",
+  projectId: "fsdworkshop",
+  storageBucket: "fsdworkshop.firebasestorage.app",
+  messagingSenderId: "610640269050",
+  appId: "1:610640269050:web:db65148f654354e7a70343",
+  measurementId: "G-JGJV6ZFH62"
 };
 
+
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const db=getFirestore(app)
 export {db}
